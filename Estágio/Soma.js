80e9652609dfa0e061e1d27768cf9a06d26eb89b
Juliis.zip <125084517+JuliisZip@users.noreply.github.com>
2024-03-13 13:56:53 -0300

    Update Soma.js

--- Estágio/Soma.js
+++ Estágio/Soma.js
@@ -1,13 +1,13 @@
-let INDICE = 13;
-let SOMA = 0;
-let K = 0;
+int main() {
+  int INDICE = 13;
+  int SOMA = 0;
+  int K = 0;
 
-while (K < INDICE) {
+  while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+printf("O valor da variavel SOMA é de: %d",SOMA);
 
-  K = 1; 
-  SOMA = SOMA + K;
+    return 0;
 }
-
-console.log(SOMA);
-
-alert("O valor da váriavel SOMA é de: " + SOMA);
